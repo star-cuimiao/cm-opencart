529efa23bc05acdc7e3c5306894559ddd613ab7d
cuimiao <1969617856@qq.com>
2020-02-24 15:58:33 +0800

    add cm_opencart

---
+++ cm_opencart/src/main/java/io/cm/cm_opencart/CmOpencartApplication.java
@@ -0,0 +1,13 @@
+package io.cm.cm_opencart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmOpencartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmOpencartApplication.class, args);
+    }
+
+}
---
+++ cm_opencart/src/test/java/io/cm/cm_opencart/CmOpencartApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cm.cm_opencart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmOpencartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
