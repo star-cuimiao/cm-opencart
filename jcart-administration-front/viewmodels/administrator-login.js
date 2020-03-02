072187d42e0c7111fe947635a9c736cf56091b9a
cuimiao <1969617856@qq.com>
2020-03-02 21:03:52 +0800

    add administrator  login and index and delete and batchDelete

--- jcart-administration-front/viewmodels/administrator-login.js
+++ jcart-administration-front/viewmodels/administrator-login.js
@@ -6,26 +6,26 @@ var app = new Vue({
     },
     methods: {
         handleLoginClick(){
-            close.log('login click');
+            console.log('login click');
             this.loginAdministrator();
         },
-        loginAdministrator(){
-            axios.get('/administrator/login',{
+        loginAdministrator() {
+            axios.get('/administrator/login', {
                 params: {
-                    username:this.username,
-                    password=this.password
+                    username: this.username,
+                    password: this.password
                 }
             })
-                .then(function (response){
+                .then(function (response) {
                     console.log(response);
                     var dto = response.data;
-                    localStorage['jcartToken']= dto.token;
-                    localStorage['expireTimestamp']= dto.expireTimestamp;
+                    localStorage['jcartToken'] = dto.token;
+                    localStorage['expireTimestamp'] = dto.expireTimestamp;
                     alert('登陆成功');
                 })
-                .catch(function(error){
+                .catch(function (error) {
                     console.log(error);
                 });
         }
-    },
+    }
 })
