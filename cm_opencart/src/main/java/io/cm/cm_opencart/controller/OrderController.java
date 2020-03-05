7be089101141a2b85c88fcfc3b280ad03992248d
cuimiao <1969617856@qq.com>
2020-03-05 18:31:39 +0800

    add administrator order getById apis

--- cm_opencart/src/main/java/io/cm/cm_opencart/controller/OrderController.java
+++ cm_opencart/src/main/java/io/cm/cm_opencart/controller/OrderController.java
@@ -29,7 +29,8 @@ public class OrderController {
 
     @GetMapping("/getById")
     public OrderShowOutDTO getById(@RequestParam Long orderId){
-            return null;
+        OrderShowOutDTO orderShowOutDTO = orderService.getById(orderId);
+        return orderShowOutDTO;
 
     }
 
--- cm_opencart/src/main/java/io/cm/cm_opencart/service/impl/OrderServiceImpl.java
+++ cm_opencart/src/main/java/io/cm/cm_opencart/service/impl/OrderServiceImpl.java
@@ -1,16 +1,21 @@
 package io.cm.cm_opencart.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import io.cm.cm_opencart.dao.OrderDetailMapper;
 import io.cm.cm_opencart.dao.OrderMapper;
 import io.cm.cm_opencart.dto.out.OrderListOutDTO;
+import io.cm.cm_opencart.dto.out.OrderProductShowOutDTO;
 import io.cm.cm_opencart.dto.out.OrderShowOutDTO;
 import io.cm.cm_opencart.po.Order;
 import io.cm.cm_opencart.po.OrderDetail;
 import io.cm.cm_opencart.service.OrderService;
+import io.cm.cm_opencart.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OrderServiceImpl implements OrderService {
@@ -19,6 +24,9 @@ public class OrderServiceImpl implements OrderService {
     private OrderMapper orderMapper;
     @Autowired
     private OrderDetailMapper orderDetailMapper;
+
+    @Autowired
+    private ProductService productService;
 
     @Override
     public Page<OrderListOutDTO> search(Integer pageNum) {
@@ -29,8 +37,29 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public OrderShowOutDTO getById(Long orderId) {
+
         Order order = orderMapper.selectByPrimaryKey(orderId);
         OrderDetail orderDetail = orderDetailMapper.selectByPrimaryKey(orderId);
-        return null;
+
+        OrderShowOutDTO orderShowOutDTO = new OrderShowOutDTO();
+        orderShowOutDTO.setOrderId(orderId);
+        orderShowOutDTO.setCustomerId(order.getCustomerId());
+        orderShowOutDTO.setStatus(order.getStatus());
+        orderShowOutDTO.setTotalPrice(order.getTotalPrice());
+        orderShowOutDTO.setRewordPoints(order.getRewordPoints());
+        orderShowOutDTO.setCreateTimestamp(order.getCreateTime().getTime());
+        orderShowOutDTO.setUpdateTimestamp(order.getUpdateTime().getTime());
+
+        orderShowOutDTO.setShipMethod(orderDetail.getShipMethod());
+        orderShowOutDTO.setShipAddress(orderDetail.getShipAddress());
+        orderShowOutDTO.setShipPrice(orderDetail.getShipPrice());
+        orderShowOutDTO.setPayMethod(orderDetail.getPayMethod());
+        orderShowOutDTO.setInvoiceAddress(orderDetail.getInvoiceAddress());
+        orderShowOutDTO.setInvoicePrice(orderDetail.getInvoicePrice());
+        orderShowOutDTO.setComment(orderDetail.getComment());
+        List<OrderProductShowOutDTO> orderProductShowOutDTOS = JSON.parseArray(orderDetail.getOrderProducts(), OrderProductShowOutDTO.class);
+        orderShowOutDTO.setOrderProducts(orderProductShowOutDTOS);
+
+        return orderShowOutDTO;
     }
 }
