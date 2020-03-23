531f1799d126865d9d9e3cfe437d37820ea4e580
cuimiao <1969617856@qq.com>
2020-03-23 21:33:19 +0800

    add product operation

---
+++ cm-store-back/src/main/java/io/cm/cmstorkback/dao/ProductOperationMapper.java
@@ -0,0 +1,17 @@
+package io.cm.cmstorkback.dao;
+
+import io.cm.cmstorkback.po.ProductOperation;
+
+public interface ProductOperationMapper {
+    int deleteByPrimaryKey(Integer productId);
+
+    int insert(ProductOperation record);
+
+    int insertSelective(ProductOperation record);
+
+    ProductOperation selectByPrimaryKey(Integer productId);
+
+    int updateByPrimaryKeySelective(ProductOperation record);
+
+    int updateByPrimaryKey(ProductOperation record);
+}
